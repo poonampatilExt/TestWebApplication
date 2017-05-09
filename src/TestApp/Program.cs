4c7a97c035df5fd87a1d4ecb65a927797dd77122
poonampatilExt <poonam.patil@extetnia.com>
2017-05-09 14:22:50 +0530

    first commit

---
+++ src/TestApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TestWebApplication.Controllers;
+using Xunit;
+
+namespace TestApp
+{
+    public class Program
+    {
+        [Fact]
+        public void TestGetMethod()
+        {
+            ValuesController valuesController = new ValuesController();
+            IEnumerable<string> result = valuesController.Get();
+
+            Assert.Equal(result.Count(), 2);
+        }
+    }
+}
